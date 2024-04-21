e76e35f8b6f5b4cfd6ac116cc2839d0947cee789
Gianluigi Forte <koalakoker@gmail.com>
2024-04-21 17:56:34 +0200

    Basic of composition

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-shapesTest();
+const p1 = new Polygon(new Point(200, 200), 100, 4, 0);
+const r1 = new Rectangle(new Point(200, 200), 100, 50, 0, new Pen("red"));
+
+const comp = new Composition();
+comp.addShape(p1);
+comp.addShape(r1);
+comp.draw();
